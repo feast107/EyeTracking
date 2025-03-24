2403c1c5787f2fe23db3306e34365d841feb6ff6
Feast <feast107@gmail.com>
2025-03-24 18:01:20 +0800

    Update native eyetracking

--- src/EyeTracking/Extensions/ServiceExtensions.cs
+++ src/EyeTracking/Extensions/ServiceExtensions.cs
@@ -5,5 +5,5 @@ namespace EyeTracking.Extensions;
 public static class ServiceExtensions
 {
     public static IServiceCollection AddEyeTrackContext(this IServiceCollection collection) =>
-        collection.AddSingleton<EyeTrackContext<EyeDetectResult>, LatestTrackContext>();
+        collection.AddSingleton<EyeTrackContext<EyeDetectResult>, NativeEyeTrackContext>();
 }
